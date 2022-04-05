4c20ea274c0a0a3efafe6d48539e9b2e38348c28
Karen <73141531+karenzhan8@users.noreply.github.com>
2022-04-05 11:40:00 -0400

    Revert "added observer and subject classes, working on implementing into existing code"
    This reverts commit bb3ed626d435f1eacdbca7ebb763209d738f3a0b.

--- src/utils/Observer.java
+++
@@ -1,6 +0,0 @@
-package utils;
-
-interface Observer {
-	  public void update(Subject subject, UserSelection selection, ExecuteTrade cumulativeTrades);
-}
-
--- src/utils/UpdateData.java
+++
@@ -1,7 +0,0 @@
-package utils;
-
-public class UpdateData extends Subject{
-	public void tick(UserSelection selection, ExecuteTrade cumulativeTrades) {
-		notifyObservers(selection, cumulativeTrades);
-	}
-}
